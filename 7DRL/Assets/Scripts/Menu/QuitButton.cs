4c9eb4a110debbbe95a4094e0973e583a82fa07e
Paisena <m8ames@gmail.com>
2026-03-02 17:42:58 -0800

    Added quit button

---
+++ 7DRL/Assets/Scripts/Menu/QuitButton.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class QuitButton : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game!");
+        Application.Quit();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
